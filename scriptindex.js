e9c3f7d54a66bbf3f91091002e3e8a85aa57fd9d
niccolotondi <niccolo.tondi@me.com>
2018-02-22 10:40:06 +0100

    ho aggiunto tutti i file

---
+++ scriptindex.js
@@ -0,0 +1,15 @@
+function loadInformation(){
+    var persona = extractRandomUser('It');
+
+    
+    
+    document.getElementById('namegen1').textContent = persona.name.first + ' ' + persona.name.last;  
+    document.getElementById('namegen2').textContent = persona.name.first + ' ' + persona.name.last;  
+    document.getElementById('figuregen').src = persona.picture.large; 
+    document.getElementById('addressgen').textContent = persona.location.street + ', ' + persona.location.city + ' ' + persona.location.postcode + ', ' + persona.location.state; 
+    document.getElementById('emailgen').textContent = persona.email;   
+    document.getElementById('phonegen').textContent = persona.phone;   
+    
+}
+
+
